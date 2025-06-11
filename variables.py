58f8aef5149918d3a0ef73a2a53cfd6781485976
PHILIPB33208 <ssentongophilip08@gmail.com>
2025-06-11 12:51:57 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,33 @@
+## Assigning  (differentiate between Assigning and declarin)
+## declaring
+
+## value
+
+## data types
+"""
+Int
+strings/characters
+flaot
+boolen
+"""
+
+number_one =30 # int
+number_two = 3.27 # flaot
+number_three ='5' # str
+is_attentive = False
+is_present = True
+
+## checking for datatype
+print(type(number_one))
+print(type(number_two))
+print(type(number_three))
+print(type(is_attentive))
+print(type(is_present))
+
+## coversions
+new_number_two = int(number_two)
+print(f"integer: {new_number_two} datatype: {type(number_two)}")
+age ="8" ##convert to integer
+string_int = "8"
+string_int = int(string_int)
+print(string_int)
