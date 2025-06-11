6b49d593f043ad1009da052748ebef2000f0de54
PHILIPB33208 <ssentongophilip08@gmail.com>
2025-06-11 12:50:56 +0300

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,21 @@
+# (- single line comment)
+"""
+Muti line comment
+
+this is another commented line
+"""
+
+
+# print("this is my name")
+
+# pythion codiong case (snake case)
+"""
+first_name
+mukisa_dan
+pops_sky
+
+"""
+
+# Indent
+first_name ="chales"
+print(first_name)
