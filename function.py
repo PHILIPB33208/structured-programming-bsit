0fa6dda7406e4d8585a7e63362cb723fe64484f3
PHILIPB33208 <ssentongophilip08@gmail.com>
2025-06-18 23:36:43 -0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,31 @@
+user_name ="ssentongo philip"
+print(f"Goodmorning, {user_name}")
+
+user_mame = "ssentongo philip"
+print(f"Goodmorning, {user_name}")
+
+sum = 1 + 4
+print(f"{1} plus f{4} equals to {sum}")
+
+user_name = "philip"
+print(f"good evening, {user_name}")
+
+def function_name():
+    return "whatever you feel like !"
+print(function_name())
+
+def sum_of_numbers(a,b):
+    return a + b
+print(sum_of_numbers(8,8))
+
+def square_of_number(x):
+    return x * x
+
+print("happy_always", "keep_the_smile", sep = "/t")
+
+x =7
+new_x = int(x)
+new_x = float(x)
+new_x = str(x)
+
+
